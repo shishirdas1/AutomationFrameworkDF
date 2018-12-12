2c07dca76e249b2ecba546cdf9702ed7e74f972f
shishirdas1 <shishir.4444@gmail.com>
2018-12-12 19:02:05 +0530

    updated framework

--- automationqa/src/test/java/com/lh/test/CommunityTest.java
+++ automationqa/src/test/java/com/lh/test/CommunityTest.java
@@ -1,13 +1,11 @@
 package com.lh.test;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.Assertion;
 import com.lh.pages.authenticated.LogOutPage;
@@ -15,11 +13,8 @@ import com.lh.pages.authenticated.MainPage;
 import com.lh.pages.community.CommunityPage;
 import com.lh.pages.unauthenticated.LoginPage;
 import com.lh.test.base.BaseTestClass;
-import com.lh.utils.UtilityForDataProvider;
+import com.lh.utils.DataProviders;
 import com.lh.utils.UtilityMethods;
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
 
 /**
  * <h2>Define test for Community here!</h2>
@@ -53,8 +48,7 @@ public class CommunityTest extends BaseTestClass {
 	boolean linkNotPresent;
 	ArrayList<String> topicNameList = new ArrayList<String>();
 	ArrayList<String> topicDescriptionList = new ArrayList<String>();
-	static ExtentTest test;
-	static ExtentReports report;
+
 
 	@BeforeClass
 	public void initClass() {
@@ -63,12 +57,10 @@ public class CommunityTest extends BaseTestClass {
 		logoutPage = new LogOutPage();
 		mainPage = new MainPage();
 		communityPage = new CommunityPage();
-		report = new ExtentReports(System.getProperty("user.dir") + "\\ExtentReportResults.html");
-		test = report.startTest("CommunityTest");
 
 	}
 
-	@Test(dataProvider = "CommunityDataProvider", priority = 1)
+	@Test(dataProvider = "CommunityDataProvider", dataProviderClass=DataProviders.class, priority = 1)
 	public void multipleValidLogin(String userName, String password) throws InterruptedException {
 		loginPage.loginAs(userName, password);
 		logoutPage.clickLogoutLink("portal");
@@ -78,10 +70,7 @@ public class CommunityTest extends BaseTestClass {
 	public void verifyGroupNameFieldIsRequired() {
 		loginPage.loginAs(communityData.get(VALID_USERID), communityData.get(VALID_PASSWORD));
 		mainPage.clickCommunity();
-		if (communityPage.GroupNameFieldIsRequired(communityData.get(ERROR_MESSAGE_GROUP))) {
-			test.log(LogStatus.PASS, "New group name field is required PASSED");
-		} else
-			test.log(LogStatus.FAIL, "New group name field is required FAILED");
+		communityAssert.assertTrue(communityPage.GroupNameFieldIsRequired(communityData.get(ERROR_MESSAGE_GROUP)));
 	}
 
 	@Test(description = "Leader/Admin - Create New Group", priority = 3)
@@ -128,6 +117,7 @@ public class CommunityTest extends BaseTestClass {
 	public void verifyArrowAppearsInForums() {
 		communityAssert.assertTrue(communityPage.verifySortArrowisPresent());
 	}
+	
 
 	@Test(description = "Verify user is able to sort Topics in both ascending and descending directions.", priority = 9)
 	public void verifyTopicSorting() {
@@ -145,13 +135,6 @@ public class CommunityTest extends BaseTestClass {
 		logoutPage.clickLogoutLink("portal");
 	}
 
-	@DataProvider(name = "CommunityDataProvider")
-	public Object[][] getLoginData() throws IOException {
-		UtilityForDataProvider dataProviderUtility = new UtilityForDataProvider();
-		Object data[][] = dataProviderUtility.getTestData("LH.xls", "CommunityLoginUser");
-		return data;
-	}
-
 	@AfterClass
 	public void classTearDown() {
 		logger.info("Inside the classTearDown() method for Community class...");
@@ -159,8 +142,6 @@ public class CommunityTest extends BaseTestClass {
 		logoutPage = null;
 		mainPage = null;
 		communityPage = null;
-		report.endTest(test);
-		report.flush();
 		logger.info("Writing the output to the Result output text file.");
 		logger.info("Exiting the classTearDown() method for Community class \n\n");
 
---
+++ automationqa/src/test/java/com/lh/utils/DataProviders.java
@@ -0,0 +1,46 @@
+package com.lh.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.testng.annotations.DataProvider;
+
+public class DataProviders {
+
+	@DataProvider(name = "CommunityDataProvider")
+	public Object[][] getLoginData() throws IOException {
+		Object data[][] = getTestData("LH.xls", "CommunityLoginUser");
+		return data;
+	}
+
+	public Object[][] getTestData(String workBookName, String sheetName) throws IOException {
+		HSSFWorkbook workbook = new HSSFWorkbook(
+				new FileInputStream(new File(System.getProperty("user.dir") + "/src/test/resources/" + workBookName)));
+		HSSFSheet worksheet = workbook.getSheet(sheetName);
+
+		int rowcount = worksheet.getPhysicalNumberOfRows();
+
+		Object[][] data = new Object[rowcount][2];
+
+		for (int i = 0; i < rowcount; i++) {
+
+			HSSFRow row = worksheet.getRow(i);
+			HSSFCell username = row.getCell(0);
+			data[i][0] = username.getStringCellValue();
+
+			HSSFCell password = row.getCell(1);
+			data[i][1] = password.getStringCellValue();
+		}
+		workbook.close();
+		workbook = null;
+		worksheet = null;
+		System.out.println(data);
+		return data;
+
+	}
+}
